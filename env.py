d4f9c701e2612c27fad4d0678caaa969fea4394a
bambshot <takeshima.nitkc@gmail.com>
2021-05-10 03:54:46 +0900

    [add] add enviroment files

---
+++ env.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+OWM_API_KEY = os.environ.get("OWM_API_KEY")
