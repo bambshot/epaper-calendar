e53a0f8e9c3c835ba2d9301c9d310f57bc0f63c5
tabo-syu <takeshima.nitkc@gmail.com>
2022-08-13 06:52:27 +0000

    [fix] fix env

--- env.py
+++ env.py
@@ -11,3 +11,4 @@ dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 OWM_API_KEY = os.environ.get("OWM_API_KEY")
+GOOGLE_CALENDAR_API_KEY = os.environ.get("GOOGLE_CALENDAR_API_KEY")
